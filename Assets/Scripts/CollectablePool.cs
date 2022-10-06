217ea4e12486a68857954513b2770454a7b7b4d3
Ege Oral <egeoral@gmail.com>
2022-10-06 21:28:21 +0300

    Collectable pool added.

---
+++ Assets/Scripts/CollectablePool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablePool : MonoBehaviour
+{
+    private int counter = 0;
+
+    private void OnTriggerEnter(Collider other) {
+        counter += 1;
+    }
+
+    private void Update() {
+        print(counter);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Camera mainCamera;
-    // Start is called before the first frame update
+    Rigidbody playerRigidBoyd;
+
+    [SerializeField] float playerSpeed = 10f;
+
+    private bool startPlaying = false;
+    private bool isReachedStopPoint = false;
+
+
     void Start()
     {
         mainCamera = Camera.main;
+        playerRigidBoyd = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.touchCount > 0)
         {
+            print("touched");
             Touch touch = Input.GetTouch(0);
             if(touch.phase == TouchPhase.Moved)
             {
+                startPlaying = true;
                 Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10f));
-                transform.position = Vector3.Lerp(transform.position, new Vector3(worldPosition.x * 2, 0, 0), 50f * Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, 
+                                                  new Vector3(worldPosition.x, transform.position.y, transform.position.z), 
+                                                  20f * Time.deltaTime);
             }
-            
-
         }
+
+        if(startPlaying && !isReachedStopPoint)
+            transform.position += transform.forward * Time.deltaTime * playerSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        isReachedStopPoint = true;
     }
 }
