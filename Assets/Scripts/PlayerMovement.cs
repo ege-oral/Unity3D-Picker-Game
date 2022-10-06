7b49584011d3f7f70b676cd32fae73866eb4441c
Ege Oral <egeoral@gmail.com>
2022-10-06 18:12:58 +0300

    Player movement added.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10f));
+                transform.position = Vector3.Lerp(transform.position, new Vector3(worldPosition.x * 2, 0, 0), 50f * Time.deltaTime);
+            }
+            
+
+        }
+    }
+}
