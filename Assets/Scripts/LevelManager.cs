a0987fb1a751f60ccb0273ee7ab52e3916f187b9
Ege Oral <egeoral@gmail.com>
2022-10-08 17:04:11 +0300

    Disable previous levels functionality, added.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,9 +5,12 @@ using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] levelStartPositions;
     [SerializeField] TextMeshProUGUI levelIndicator;
     [SerializeField] GameObject player;
+
+    [SerializeField] GameObject levels;
+    private List<GameObject> levelsStartPositions = new List<GameObject>();
+    private int previousLevelCount = 0;
 
     private void Awake() 
     {
@@ -16,13 +19,19 @@ public class LevelManager : MonoBehaviour
         
         int Level_Number = PlayerPrefs.GetInt("Level_Number");
 
-        for(int i = 0; i < Level_Number - 1; i++)
+        foreach(Transform level in levels.transform)
         {
-            levelStartPositions[i].SetActive(false);
+            if(previousLevelCount < Level_Number - 1)
+            {
+                level.gameObject.SetActive(false);
+            }
+            levelsStartPositions.Add(level.gameObject);
+            previousLevelCount += 1;
         }
 
-        player.transform.position = levelStartPositions[Level_Number - 1].transform.position;
+        player.transform.position = levelsStartPositions[Level_Number - 1].transform.position;
     }
+
 
     private void Update() 
     {
